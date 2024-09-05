19ba298cfe8d166eb6564f9022a436be957ebf41
cyberl-ty02 <lty20205281@stud.tjut.edu.cn>
2024-09-05 11:12:43 +0800

    delete some unnecessary files · 1b3b3b3b7b - pims

--- Java/IdeaProjects/pims/src/com/tjut/util/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.tjut.util;
-
-public class Constant {
-	public static final String RESPONSE_CODE_SUCCESS = "0";
-	public static final String RESPONSE_CODE_DB_FAILURE = "1";
-	public static final String RESPONSE_CODE_DUPLICATE = "2";
-	
-}
--- Java/IdeaProjects/pims/src/com/tjut/util/Uuid.java
+++
@@ -1,9 +0,0 @@
-package com.tjut.util;
-
-import java.util.UUID;
-
-public class Uuid {
-	public static String get(){
-		return UUID.randomUUID().toString().replace("-", "");
-	}
-}
